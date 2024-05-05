2203e0a334e6e5ab1681222332d3325782f1a5fc
saddiq-dev <saddiqa.qadir@gmail.com>
2024-05-04 21:24:18 -0400

    Installed Tailwind in the project

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,5 +1,11 @@
 import "./index.css";
 
 export default function App() {
-  return <h1>Hey</h1>;
+  return (
+    <header>
+      <h1 className="pt-3.5 font-serif text-slate-900">
+        TaskMaster: Your Ultimate To-Do List Companion ✔️
+      </h1>
+    </header>
+  );
 }
