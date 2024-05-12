4c81e2e0348bd277a8e802cbf0301ce2b8b84e9f
saddiq-dev <saddiqa.qadir@gmail.com>
2024-05-12 17:40:05 -0400

    Created the list, buttons and stats components.

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,10 +1,15 @@
 import "./index.css";
 
+const tasks = [{ task: "Clean house" }, { task: "Go to Gym" }];
+
 export default function App() {
   return (
     <>
       <Header />
       <Form />
+      <List />
+      <Buttons />
+      <Stats />
     </>
   );
 }
@@ -31,6 +36,35 @@ function Form() {
           Add ✅
         </button>
       </form>
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <div className="list">
+      <div className="list-item">
+        <input type="checkbox"></input>
+        <p>Item 1</p>
+        <button>❌</button>
+      </div>
+    </div>
+  );
+}
+
+function Buttons() {
+  return (
+    <div className="buttons">
+      <button className="btn">Sort by Input Date</button>
+      <button className="btn">Clear List</button>
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <div className="stats">
+      <p>70% of your tasks are completed </p>
     </div>
   );
 }
